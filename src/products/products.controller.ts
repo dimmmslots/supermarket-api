d05e2173b09791abf7062cb3eeb990138c31722f
dimmmslots <vctrline2048@gmail.com>
2022-08-16 02:46:30 +0700

    pagination and get product by id endpoint done

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,17 +6,35 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiParam } from '@nestjs/swagger';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Get()
-  async getAllProducts() {
-    return this.productsService.getAllProducts();
+  @ApiParam({
+    name: 'page',
+    description: 'Page number',
+    allowEmptyValue: true,
+    type: Number,
+  })
+  async getAllProducts(@Query() query) {
+    return this.productsService.getAllProducts(query.page);
+  }
+
+  @Get(':id')
+  async getProduct(@Param('id') id: number) {
+    if (isNaN(id)) {
+      return {
+        error: 'Invalid id',
+      };
+    }
+    return this.productsService.getProduct(id);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -7,7 +7,28 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class ProductsService {
   constructor(private readonly prisma: PrismaService) {}
 
-  getAllProducts() {
-    return this.prisma.product.findMany();
+  getAllProducts(page: number) {
+    if (isNaN(page) || !page) {
+      return this.prisma.product.findMany({ take: 20 });
+    }
+
+    return this.prisma.product.findMany({
+      skip: --page * 20,
+      take: 20,
+    });
+  }
+
+  async getProduct(id: number) {
+    const parsedId = parseInt(id.toString(), 10);
+    const data = await this.prisma.product.findUnique({
+      where: { id: parsedId },
+    });
+    // check if data is valid
+    if (!data) {
+      return {
+        error: 'Product not found',
+      };
+    }
+    return data;
   }
 }
